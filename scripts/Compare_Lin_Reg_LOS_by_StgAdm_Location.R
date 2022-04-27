625be28c1498f26cd59a6ed9569e5c7b70eab311
Steph Reynolds <sreynolds1027@gmail.com>
2022-04-26 17:35:45 -0700

    Added line to scripts to remove last 2 rows of raw data files

--- scripts/Compare_Lin_Reg_LOS_by_StgAdm_Location.R
+++ scripts/Compare_Lin_Reg_LOS_by_StgAdm_Location.R
@@ -1,17 +1,22 @@
 # Linear Regression (LOS ~ stage_adm) by Location (UCSF vs. UTSW)
 
-# Import utsw dataset
-ut <- readRDS("/Users/sreynolds2/Documents/GitHub/MS-Covid_Staging/data/UTSW/dm_stg_unique_pts.rds")
+# Import utsw data
+ut <- readRDS("/Users/sreynolds2/Documents/GitHub/MS-UTSW_Covid_Staging/data/ut_df_unique_pts.rds")
 
-# Import ucsf data; remove DOD and smoking vars
-uc <- readRDS("/Users/sreynolds2/Documents/GitHub/MS-Covid_Staging/data/dm_stg_unique_pt_11.08.21.rds") %>%
-  select(-c(DOD, smoking)) 
+# Import ucsf data and remove unnecessary variables
+uc <- readRDS("/Users/sreynolds2/Documents/GitHub/MS-Covid_Staging/data/covid_dm_stages_unique_FINAL_4.14.22.rds") %>%
+  select(-c(marital_status, language, smoking, date_adm, date_disc)) 
+
+# For UTSW, change max_stage, stage_adm, and stage_disc variables to ORDERED factor type
+ut$max_stage <- factor(ut$max_stage, ordered = T, levels = c("4", "5", "6", "7", "8", "9", "10"))
+ut$stage_adm <- factor(ut$stage_adm, ordered = T, levels = c("4", "5", "6", "7", "8", "9"))
+ut$stage_disc <- factor(ut$stage_disc, ordered = T, levels = c("4", "5", "6", "7", "8", "9", "10"))
 
 # Merge datasets using location (loc) as identifier 
 combined <- bind_rows("UTSW" = ut, "UCSF" = uc, .id = "loc") %>% 
   relocate(loc, .after = "ID")
 
-# Load packages required for multiple linear regressions 
+# Load packages required for multiple linear regression 
 library(tidymodels)
 library(tidyquant)
 library(broom)
@@ -19,6 +24,7 @@ library(purrr)
 library(tidyverse)
 
 # Group split by location `loc` to create iterables for UCSF and UTSW, then generate linear regression for each iterable
+# LOS ~ stage_adm
 combined %>% 
   mutate(loc = as.factor(loc)) %>% 
   group_by(loc) %>% 
@@ -30,7 +36,6 @@ combined %>%
       glance() %>% 
       add_column(loc = unique(df$loc), .before = 1)
   })
-
 
 ####
 # GROUP SPLIT BY STAGE_ADM
@@ -45,8 +50,3 @@ combined %>%
       add_column(stage_adm = unique(df$stage_adm), .before = 1)
   })
   
-
-
-
-
-
